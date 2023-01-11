7e42dc49723b7a57991ff2b3f7b534277d1ee5d4
kmarray98 <77078860+kmarray98@users.noreply.github.com>
2023-01-11 11:15:08 +0000

    Deploying to gh-pages from  @ fa6f7ebb26d0084ac3f9a53400fa89ee2a1130a1 🚀

---
+++ notebooks/notebooks_2_Kline.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 148e0500-91a0-11ed-0f77-6da06edf8436
+md"# Place-based policies - Kline and Moretti (2014)"
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─f5450eab-0f9f-4b7f-9b80-992d3c553ba9
+# ╠═148e0500-91a0-11ed-0f77-6da06edf8436
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
---
+++ notebooks/notebooks_2_rossi_hanberg.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c9afc280-919f-11ed-069b-6b4626976475
+md" # Place-based policies: Rossi-Hanberg et al. (2010)"
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─f5450eab-0f9f-4b7f-9b80-992d3c553ba9
+# ╠═c9afc280-919f-11ed-069b-6b4626976475
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
