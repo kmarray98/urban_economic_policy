4d7c5164a102813abaedf4fc3b7c63c3bcc55fd9
Kieran Marray <77078860+kmarray98@users.noreply.github.com>
2023-01-11 12:08:58 +0100

    Add files via upload

---
+++ notebooks/1_rosen.jl
@@ -0,0 +1,28 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 97b82a10-919f-11ed-3013-07f52473d7d9
+md" # Hedonic pricing -- Rosen (1974)"
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─97b82a10-919f-11ed-3013-07f52473d7d9
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
